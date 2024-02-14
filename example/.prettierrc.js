38cb3d7895766f43e74893a1da9c420865801f4a
Sagar Pawar <sagar.pawar@simformsolutions.com>
2024-02-14 17:16:59 +0530

    feat(UNT-T22114): Update RN version

--- example/.prettierrc.js
+++ example/.prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: true,
+  bracketSpacing: false,
   singleQuote: true,
-  trailingComma: 'none',
+  trailingComma: 'all',
 };
--- example/index.js
+++ example/index.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
-import 'react-native-gesture-handler';
-import { name as appName } from './app.json';
+import {AppRegistry} from 'react-native';
+// import 'react-native-gesture-handler';
+import {name as appName} from './app.json';
 import App from './src/App';
 
 AppRegistry.registerComponent(appName, () => App);
